61df6aa567ab28b9857e0a90c0a78f1055e5b0ee
Oleg <vishnevskiyoleg@gmail.com>
2023-10-24 17:02:19 +0300

    game ui 2

--- app/src/main/java/com/snapd/PlinApp.kt
+++ app/src/main/java/com/snapd/PlinApp.kt
@@ -3,7 +3,6 @@ package com.snapd
 import android.app.Application
 import com.snapd.di.dataModule
 import com.snapd.di.domainModule
-import com.snapd.di.presentationModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -17,7 +16,7 @@ class PlinApp : Application() {
         startKoin {
             androidLogger(level = Level.ERROR)
             androidContext(this@PlinApp)
-            modules(listOf(domainModule, dataModule, presentationModule))
+            modules(listOf(domainModule, dataModule))
         }
     }
 }
--- app/src/main/java/com/snapd/presantation/viewModels/JumpGameViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.snapd.presantation.viewModels
-
-import androidx.lifecycle.ViewModel
-
-class JumpGameViewModel : ViewModel() {
-
-
-}
